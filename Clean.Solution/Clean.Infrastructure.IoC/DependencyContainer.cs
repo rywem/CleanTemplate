82acdbf0050717bd7a0c8d6dfc60f87ae1fa6d49
Ryan <cybervoid41@gmail.com>
2020-10-24 15:41:19 -0700

    DependencyContainer

---
+++ Clean.Solution/Clean.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using Clean.ApplicationCore.Interfaces;
+using Clean.ApplicationCore.Services;
+using Clean.Domain.Interfaces;
+using Clean.Infrastructure.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services )
+        {
+            // Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Infrastructure.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+        }
+    }
+}
