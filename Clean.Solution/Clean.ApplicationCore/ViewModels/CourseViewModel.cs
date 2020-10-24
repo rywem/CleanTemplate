21027f2344058e89086e566ec862cf648d4aedbf
Ryan <cybervoid41@gmail.com>
2020-10-24 15:04:01 -0700

    ApplicationCore ICourseService, ICourseRepository in Domain

---
+++ Clean.Solution/Clean.ApplicationCore/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Clean.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.ApplicationCore.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
