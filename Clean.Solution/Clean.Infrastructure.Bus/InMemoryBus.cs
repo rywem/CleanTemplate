212941b90764e3f73215dfb519493f831c728624
Ryan <cybervoid41@gmail.com>
2020-10-25 06:00:57 -0700

    Adding Clean.Domain.Core and implementation of InMemoryBus

--- Clean.Solution/Clean.Infrastructure.Bus/InMemoryBus.cs
+++ Clean.Solution/Clean.Infrastructure.Bus/InMemoryBus.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Clean.Domain.Core.Bus;
+using Clean.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Clean.Infrastructure.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus( IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>( T command ) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
