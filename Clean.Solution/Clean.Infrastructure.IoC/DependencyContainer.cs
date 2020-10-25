defe6a0628bc307dc9add5be1ad3e41a22797b0a
Ryan <cybervoid41@gmail.com>
2020-10-25 06:28:54 -0700

    DependencyContainer expanded.

--- Clean.Solution/Clean.Infrastructure.IoC/DependencyContainer.cs
+++ Clean.Solution/Clean.Infrastructure.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using Clean.ApplicationCore.Interfaces;
 using Clean.ApplicationCore.Services;
+using Clean.Domain.CommandHandlers;
+using Clean.Domain.Commands;
+using Clean.Domain.Core.Bus;
 using Clean.Domain.Interfaces;
+using Clean.Infrastructure.Bus;
+using Clean.Infrastructure.Data.Context;
 using Clean.Infrastructure.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,17 @@ namespace Clean.Infrastructure.IoC
     {
         public static void RegisterServices(IServiceCollection services )
         {
+            // Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            // Domain handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             // Application Layer
             services.AddScoped<ICourseService, CourseService>();
-
             // Infrastructure.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<SqlDbContext>();
 
         }
     }
